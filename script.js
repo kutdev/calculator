2089063859a4b58d6befe5ccd9ed7a93421bd18e
kutdev <78854154+kutdev@users.noreply.github.com>
2022-03-08 23:08:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+function add(){
+    var data2 = document.getElementById("data2").value;
+    var data1 = document.getElementById("data1").value;
+    var result = parseInt(data1) + parseInt(data2);
+    document.getElementById("result").innerHTML = result;
+
+}
+function sub(){
+    var data2 = document.getElementById("data2").value;
+    var data1 = document.getElementById("data1").value;
+    var result = parseInt(data1) - parseInt(data2);
+    document.getElementById("result").innerHTML = result;
+
+}
+function mul(){
+    var data2 = document.getElementById("data2").value;
+    var data1 = document.getElementById("data1").value;
+    var result = parseInt(data1) * parseInt(data2);
+    document.getElementById("result").innerHTML = result;
+
+}
+function div(){
+    var data2 = document.getElementById("data2").value;
+    var data1 = document.getElementById("data1").value;
+    var result = parseInt(data1) / parseInt(data2);
+    document.getElementById("result").innerHTML = result;
+
+}
+function ac(){
+    document.getElementById("data2").value = "";
+    document.getElementById("data1").value = "";
+}
+function per(){
+    var data1 = document.getElementById("data1").value;
+    document.getElementById("data2").style.visibility = "hidden";
+    var result = parseInt(data1) / 100;
+    document.getElementById("result").innerHTML = result;
+    
+    
+}
